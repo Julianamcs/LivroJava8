e16571e07e4db8b227660b81dcafad6ab552ffb6
JulianaMCS <julyanamcs@gmail.com>
2024-07-01 17:58:27 -0300

    Create EscapeString.java

---
+++ EscapeString.java
@@ -0,0 +1,12 @@
+/*
+ * Demonstra seuqu«encias de escape de strings*/
+public class EscapeString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Frist line \nSecund line");
+		System.out.println("A\tB\tC");
+		System.out.println("D\tE\tF");
+	}
+
+}
