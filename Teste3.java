a1ad033e81f6a265c0c672ba04efd650b6ad4177
JulianaMCS <julyanamcs@gmail.com>
2024-07-01 17:03:47 -0300

    Create Teste3.java

---
+++ Teste3.java
@@ -0,0 +1,16 @@
+/*
+ *Calcule a sistâ,cia da queda de um raio cujo som leve
+ *7,2 segundos para alcaça-lo */
+public class Teste3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double dist;
+		
+		dist = 7.2 * 1100;
+				
+		System.out.println("the lightning is " + dist + " feet away");
+	}
+
+}
