08c101b42475875b9dd13b2e57aa0d7cf57fc59b
JulianaMCS <julyanamcs@gmail.com>
2024-07-01 16:24:04 -0300

    Create VarCaracteresComoInteira.java

---
+++ VarCaracteresComoInteira.java
@@ -0,0 +1,22 @@
+/*
+ * Variaveis de caracteres podem ser tratadas como inteiros*/
+public class VarCaracteresComoInteira {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch;
+		ch = 'x';
+		System.out.println("ch containe " + ch);
+		
+		ch++; //incrementa ch
+		System.out.println("ch is now " + ch);
+		
+		ch = 90;//da o valor inteiro 
+		System.out.println("ch is now " + ch);
+		
+		
+		
+	}
+
+}
