9af2ff57a1b4bf64e32fb9f5f01bd6cc10ff75b1
JulianaMCS <julyanamcs@gmail.com>
2024-07-01 18:11:39 -0300

    Create InicializacaoDinamica.java

---
+++ InicializacaoDinamica.java
@@ -0,0 +1,15 @@
+/*
+ * Calcula o volume de cilindro dado o raio da sua base e sua altura*/
+public class InicializacaoDinamica {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 4, height = 5;
+		
+		//inicializa volume dinamicamente
+		double volume = 3.1416 * radius * height;
+		
+		System.out.println("Volume is " + volume);
+	}
+
+}
