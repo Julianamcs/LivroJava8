eb54402b8e66a3dffc2efab8b7c8d7045966a026
JulianaMCS <julyanamcs@gmail.com>
2024-07-01 16:02:56 -0300

    Create Pitagoras.java

---
+++ Pitagoras.java
@@ -0,0 +1,19 @@
+/*
+ * Usa o teorema de Pitágoras para encontrar o comprimento
+ * da hipotenusandados os compromentos do dois lados opostos*/
+public class Pitagoras {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double x, y, z;
+		
+		x = 3;
+		y = 4;
+		
+		z = Math.sqrt(x*x + y*y);
+		
+		System.out.println("hypotenuse is " + z);
+	}
+
+}
