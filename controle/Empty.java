8476a31a7914323ffaf09545600c11c42d3a9f85
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 01:54:58 -0300

    Create Empty.java

---
+++ controle/Empty.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * partes for podem estar vazias
+ */
+public class Empty {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for(i= 0; i < 10;) {
+			System.out.println("Pass # " + i);
+			i++;
+		}
+	}
+
+}
