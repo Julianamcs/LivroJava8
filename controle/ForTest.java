d459b5ccdb01727fb999ff7d9e94473f6184e2ef
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 00:54:34 -0300

    Create ForTest.java

---
+++ controle/ForTest.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * Executa o laço até o S ser digitado
+ */
+public class ForTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws java.io.IOException{
+		// TODO Auto-generated method stub
+		int i;
+		
+		System.out.println("Press S to stop");
+		
+		for (i = 0; (char) System.in.read() != 'S'; i++) {
+			System.out.println("Pass #" + i);
+		}
+	}
+
+}
