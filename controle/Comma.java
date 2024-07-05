e5593e737e5e323fdf5cd3ef5121c5cbe5b00326
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 00:45:45 -0300

    Create Comma.java

---
+++ controle/Comma.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * use virgulas em uma isntrução for
+ */
+public class Comma {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		
+		for (i=0, j=10; i < j; i++, j--) {
+			System.out.println("i and j: " + i + " " + j);
+		}
+	}
+
+}
