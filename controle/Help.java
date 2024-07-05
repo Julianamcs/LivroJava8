c877723605ed0d1249725e4e1749472dd895d65a
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 00:17:12 -0300

    Update Help.java

--- controle/Help.java
+++ controle/Help.java
@@ -22,6 +22,25 @@ public class Help {
 		choice = (char) System.in.read();
 		
 		System.out.println("\n");
+		
+		switch (choice) {
+		case '1':
+			System.out.println("The if:");
+			System.out.println("if(condition) statement;");
+			System.out.println("else statement;");
+			break;
+		case '2':
+			System.out.println("The switch:");
+			System.out.println("switch(expression){");
+			System.out.println(" case constant:");
+			System.out.println(" statement sequence");
+			System.out.println(" break;");
+			System.out.println(" //...");
+			System.out.println(" }");
+			break;
+		default:
+			System.out.print("Selection not found.");
+		}
 	}
 
 }
