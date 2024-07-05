7369a17272e2707b1c9a61c8e32023d9676146b0
JulianaMCS <julyanamcs@gmail.com>
2024-07-04 21:28:37 -0300

    Create Ltod.java

---
+++ Ltod.java
@@ -0,0 +1,15 @@
+/*
+ * Demonstra a conversão automática de Long para Double*/
+public class Ltod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long L = 100123285L;
+		double D;
+		
+		D = L;
+		
+		System.out.println("L and D: " + L + " " + D);
+	}
+
+}
