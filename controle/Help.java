bdfee20f9b08138b63db6b744bea0d82d5872a40
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 02:57:46 -0300

    Update Help.java

--- controle/Help.java
+++ controle/Help.java
@@ -13,16 +13,23 @@ public class Help {
 	 */
 	public static void main(String[] args) throws java.io.IOException{
 		// TODO Auto-generated method stub
-		char choice;
-		
-		System.out.println("Help on: ");
-		System.out.println("1. if");
-		System.out.println("2. switch");
-		System.out.println("Choose one: ");
-		choice = (char) System.in.read();
+		char choice, ignore;
+		do {
+			System.out.println("Help on: ");
+			System.out.println("1. if");
+			System.out.println("2. switch");
+			System.out.println("3. for");
+			System.out.println("4. while");
+			System.out.println("5. do-while");
+			System.out.println("Choose one: ");
+			choice = (char) System.in.read();
+			
+			do {
+				ignore = (char) System.in.read();
+			}while(ignore != '\n');
+		}while(choice < '1' | choice > '5');
 		
 		System.out.println("\n");
-		
 		switch (choice) {
 		case '1':
 			System.out.println("The if:");
@@ -37,6 +44,22 @@ public class Help {
 			System.out.println(" break;");
 			System.out.println(" //...");
 			System.out.println(" }");
+			break;
+		case '3':
+			System.out.println("The for:");
+			System.out.println("for(init; condition; iteration){");
+			System.out.println(" statement;");
+			System.out.println(" }");
+			break;
+		case '4':
+			System.out.println("The while:");
+			System.out.println("while(condition) statement;");
+			break;
+		case '5':
+			System.out.println("The do-while:");
+			System.out.println("do { ");
+			System.out.println(" statement; ");
+			System.out.println(" } while(condition);");
 			break;
 		default:
 			System.out.print("Selection not found.");
