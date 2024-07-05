e7a8c3359560abd5360cd49e2ec51f09a3084a59
JulianaMCS <julyanamcs@gmail.com>
2024-07-05 00:39:54 -0300

    Create DecrFor.java

---
+++ controle/DecrFor.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * um laço for sendo executado decrementando --
+ */
+public class DecrFor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		
+		for (x = 100; x > -100; x -= 5) {
+			System.out.println(x);
+		}
+			
+	}
+
+}
