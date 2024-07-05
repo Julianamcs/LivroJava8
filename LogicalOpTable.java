9a7ec0ff03a837eb35c73598d4fca404a023398c
JulianaMCS <julyanamcs@gmail.com>
2024-07-04 23:43:47 -0300

    Ajuste da tabela logica

--- LogicalOpTable.java
+++ LogicalOpTable.java
@@ -5,27 +5,27 @@ public class LogicalOpTable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean p, q;
-		System.out.println("P/t Q/t AND/t OR/t XOR/t NOT");
+		System.out.println("P\t Q\t AND\t OR\t XOR\t NOT");
 		
 		 p = true; q = true;
-		 System.out.println(p + "/t " + q + "/t ");
-		 System.out.println((p & q) + "/t " + (p|q) + "/t ");
-		 System.out.println((p ^ q) + "/t " + (!p));
-		 
+		 System.out.print(p + "\t " + q + "\t ");
+		 System.out.print((p & q) + "\t " + (p|q) + "\t ");
+		 System.out.print((p ^ q) + "\t " + (!p));
+		 System.out.println("\n");
 		 p = true; q = false;
-		 System.out.println(p + "/t " + q + "/t ");
-		 System.out.println((p & q) + "/t " + (p|q) + "/t ");
-		 System.out.println((p ^ q) + "/t " + (!p));
-		 
+		 System.out.print(p + "\t " + q + "\t ");
+		 System.out.print((p & q) + "\t " + (p|q) + "\t ");
+		 System.out.print((p ^ q) + "\t " + (!p));
+		 System.out.println("\n");
 		 p = false; q = true;
-		 System.out.println(p + "/t " + q + "/t ");
-		 System.out.println((p & q) + "/t " + (p|q) + "/t ");
-		 System.out.println((p ^ q) + "/t " + (!p));
-		 
+		 System.out.print(p + "\t " + q + "\t ");
+		 System.out.print((p & q) + "\t " + (p|q) + "\t ");
+		 System.out.print((p ^ q) + "\t " + (!p));
+		 System.out.println("\n");
 		 p = false; q = false;
-		 System.out.println(p + "/t " + q + "/t ");
-		 System.out.println((p & q) + "/t " + (p|q) + "/t ");
-		 System.out.println((p ^ q) + "/t " + (!p));
+		 System.out.print(p + "\t " + q + "\t ");
+		 System.out.print((p & q) + "\t " + (p|q) + "\t ");
+		 System.out.print((p ^ q) + "\t " + (!p));
 	}
 
 }
---
+++ controle/Help.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package controle;
+
+/**
+ * um sistema de ajuda simples
+ */
+public class Help {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws java.io.IOException{
+		// TODO Auto-generated method stub
+		char choice;
+		
+		System.out.println("Help on: ");
+		System.out.println("1. if");
+		System.out.println("2. switch");
+		System.out.println("Choose one: ");
+		choice = (char) System.in.read();
+		
+		System.out.println("\n");
+	}
+
+}
